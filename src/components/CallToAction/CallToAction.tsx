40f23519373bdae5956efac6fab7ec1f72de0f3f
Onias da Rocha <onias.filho@gmail.com>
2025-03-09 22:29:25 +0000

    starts working on CTA section

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -1,7 +1,23 @@
 import "./CallToAction.css";
 
 const CallToAction = () => {
-  return <section className="call-to-action-section">Call to action</section>;
+  return (
+    <section className="call-to-action-section">
+      <div className="founder-image"></div>
+      <div className="cta-content">
+        <div className="cta-title">Be the first to test</div>
+        <div className="cta-description">
+          Hi, I'm Louis Graham, the founder of the company. Book a demo call
+          with me to become a beta tester for our app and kickstart your
+          company. Apply for access below and I’ll be in touch to schedule a
+          call.
+        </div>
+        <div className="cta-button button-primary">
+          Apply for access
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CallToAction;
