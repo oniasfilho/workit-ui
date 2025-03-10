e19484931626767e57e7ba80358c3cbf50d9e5ac
Onias da Rocha <onias.filho@gmail.com>
2025-03-10 23:55:24 +0000

    finishes footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer className="footer-seciton">Footer</footer>;
+  return <footer className="footer-section">
+    <div className="logo-footer"></div>
+    <div className="socials-footer">
+      <div className="facebook-logo social-icon"></div>
+      <div className="twitter-logo social-icon"></div>
+      <div className="instagram-logo social-icon"></div>
+    </div>
+  </footer>;
 };
 
 export default Footer;
